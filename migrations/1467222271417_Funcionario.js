07cf1788f8c5b11687ec2176dbc5a9e4fa91792b
Frederico Castro <castro.112358@gmail.com>
2016-06-29 16:17:06 -0300

    add funcionarios migration

---
+++ migrations/1467222271417_Funcionario.js
@@ -0,0 +1,23 @@
+exports.up = function(pgm) {
+  var fields = [
+    "id SERIAL PRIMARY KEY",
+    "nome VARCHAR(40) NOT NULL",
+    "sobrenome VARCHAR(40) NOT NULL",
+    "telefone VARCHAR(40)",
+    "sexo VARCHAR(40)",
+    "email VARCHAR(80) NOT NULL UNIQUE",
+    "cpf VARCHAR(20) NOT NULL UNIQUE",
+    "rg VARCHAR(20) UNIQUE",
+    "password CHAR(60) NOT NULL",
+    "carteira_de_trabalho VARCHAR(20) NOT NULL UNIQUE",
+    "data_de_nascimento TIMESTAMP",
+    "data_de_inicio_do_contrato TIMESTAMP",
+    "data_de_fim_do_contrato TIMESTAMP",
+    "papel VARCHAR(20) UNIQUE"
+  ];
+  pgm.sql('CREATE TABLE IF NOT EXISTS funcionarios(' + fields.join(", ") + ')');
+};
+
+exports.down = function(pgm) {
+  pgm.sql('DROP TABLE funcionarios');
+};
---
+++ migrations/1467226919387_PopulateFuncionario.js
@@ -0,0 +1,21 @@
+exports.up = function(pgm) {
+  var funcionario = {
+    nome: "'Admin'",
+    sobrenome: "'Systenis'",
+    telefone: "'12345678'",
+    sexo: "'masculino'",
+    email: "'admin@systenis.com'",
+    cpf: "'999999999-00'",
+    rg: "'999999999'",
+    data_de_nascimento: "'1989-01-08'",
+    password: "'123123123'",
+    carteira_de_trabalho: "'123'",
+    data_de_inicio_do_contrato: "'2015-04-02'",
+    papel: "'admin'"
+  };
+  pgm.sql('INSERT INTO funcionarios (nome, sobrenome, telefone, sexo, email, cpf, rg, data_de_nascimento, password, carteira_de_trabalho, data_de_inicio_do_contrato, papel) VALUES ( {nome}, {sobrenome}, {telefone}, {sexo}, {email}, {cpf}, {rg}, {data_de_nascimento}, {password}, {carteira_de_trabalho}, {data_de_inicio_do_contrato}, {papel});', funcionario);
+};
+
+exports.down = function(pgm) {
+
+};
