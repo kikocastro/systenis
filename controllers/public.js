031ca2893c4c908608c4d279de9c7c9d6ddc90a4
Frederico Castro <castro.112358@gmail.com>
2016-07-01 15:01:56 -0300

    add product to index

--- controllers/public.js
+++ controllers/public.js
@@ -1,9 +1,14 @@
 module.exports = function(models) {
+  var Produto = models.Produto;
   var Client = models.Client;
 
   return {
     index: function(scope) {
       scope.currentUser = scope.session.currentUser;
+
+      return Produto.all().then(function(produtos) {
+        scope.produtos = produtos;
+      });
     },
     show: function(scope) {
     },
