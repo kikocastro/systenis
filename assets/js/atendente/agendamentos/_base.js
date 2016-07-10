6ef2092ffd28ea627676771e9077004845295724
Frederico Castro <castro.112358@gmail.com>
2016-07-09 21:38:46 -0300

    fix

--- assets/js/atendente/agendamentos/_base.js
+++
@@ -1,1 +0,0 @@
-App.Atendente.Agendamentos = {};
--- assets/js/atendente/agendamentos/cancelar_agendamento.js
+++
@@ -1,7 +0,0 @@
-App.Atendente.Agendamentos.cancelarAgendamento = function() {
-	$("#cancelar-agendamento-js").click(function(ev){
-		if(!confirm("Tem certeza que quer cancelar?")) {
-			ev.preventDefault();
-		}
-	});
-}
