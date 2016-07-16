2cb4be3eaefbc1a80cac48a13c55f667481178dc
erickkenji <erick.kenji10@gmail.com>
2016-07-16 14:12:37 -0300

    edit create endereco

--- controllers/cliente/enderecos.js
+++ controllers/cliente/enderecos.js
@@ -11,22 +11,67 @@ module.exports = function(models) {
         scope.enderecos = enderecos;
       });
     },
-    show: function(scope) {
-      var enderecoId = scope.params.id;
 
-      return Endereco.find(enderecoId).then(function(endereco) {
-        scope.endereco = endereco;
-      });
-    },
     edit: function(scope) {
       var enderecoId = scope.params.id;
 
       return Endereco.find(enderecoId).then(function(endereco) {
         scope.endereco = endereco;
       });
+    },
+
+    new: function(scope) {
+
+    },
+
+    update: function(req, res) {
+      var editedEndereco = req.body.endereco;
+      var permittedParams = getPermittedParams();
+      var enderecoId = editedEndereco.id;
+
+      return Endereco.find(enderecoId)
+        .then(function(endereco) {
+
+          _.each(permittedParams, function(param) {
+            endereco[param] = editedEndereco[param];
+          });
+
+          return endereco.save();
+        })
+        .then(function(endereco) {
+          res.redirect("/cliente/enderecos/" + endereco.id);
+        });
+    },
+
+    create: function(req, res, next) {
+      var endereco = req.body.endereco;
+
+      return Endereco.create(endereco).then(function(endereco) {
+          res.redirect("/cliente/enderecos/" + endereco.id);
+        });
+    },
+
+    show: function(scope) {
+      var enderecoId = scope.params.id;
+
+      return Endereco.find(enderecoId).then(function(endereco) {
+        scope.endereco = endereco;
+      });
     }
-    
+
   };
 };
 
+function getPermittedParams() {
 
+  var permittedParams = [
+    "cep",
+    "numero",
+    "cidade",
+    "rua",
+    "complemento",
+    "estado"
+  ];
+
+  return permittedParams;
+}
