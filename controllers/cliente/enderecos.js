c8e98da86997a99947c22ada973cf9e28a669c7f
Frederico Castro <castro.112358@gmail.com>
2016-07-17 13:43:34 -0300

    allow max 3 enderecos

--- controllers/cliente/enderecos.js
+++ controllers/cliente/enderecos.js
@@ -7,6 +7,10 @@ module.exports = function(models) {
     index: function(scope) {
       var clienteId = scope.session.currentUser.id;
       
+      if(scope.query.error === 'max_number') {
+        scope.maxError = "Número máximo de endereços permitdo é 3 por cliente.";
+      }
+
       return Endereco.where({cliente_id:clienteId}).then(function(enderecos) {
         scope.enderecos = enderecos;
         scope.showNewEnderecoButton = enderecos.length < 3;
@@ -22,7 +26,15 @@ module.exports = function(models) {
     },
 
     new: function(scope) {
+      var clienteId = scope.currentUser.id;
 
+      return Endereco.where({cliente_id: clienteId}).then(function(enderecos) {
+        if (enderecos.length > 2) {
+          var errorMsg = "max_number";
+          console.log(errorMsg)
+          res.redirect("/cliente/enderecos?error=" + encodeURIComponent(errorMsg));
+        }
+      });
     },
 
     update: function(req, res) {
@@ -48,9 +60,21 @@ module.exports = function(models) {
       var endereco = req.body.endereco;
       endereco.cliente_id = req.session.currentUser.id;
 
-      return Endereco.create(endereco).then(function(endereco) {
-          res.redirect("/cliente/enderecos/" + endereco.id);
-        });
+      return Endereco.where({cliente_id: endereco.cliente_id}).then(function(enderecos) {
+        if(enderecos.length > 2) {
+          return false;
+        }
+
+        return Endereco.create(endereco);
+      })
+      .then(function(endereco) {
+        if(!endereco) {
+          var errorMsg = "max_number";
+          console.log(errorMsg)
+          res.redirect("/cliente/enderecos?error=" + encodeURIComponent(errorMsg));
+        }
+        res.redirect("/cliente/enderecos/new" + endereco.id);
+      });
     },
 
     show: function(scope) {
