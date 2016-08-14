46d13ede28a9ceea42293a13faf5d16bcd8f1905
Isabella Pimentel de Moraes <ipimentelmoraes@gmail.com>
2016-08-14 01:02:53 -0300

    add constraint on Endereço selection of Pedido to avoid errors in execution

--- migrations/1468645679809_CreatePedido.js
+++ migrations/1468645679809_CreatePedido.js
@@ -2,7 +2,7 @@ exports.up = function(pgm) {
   var fields = [
     "id SERIAL PRIMARY KEY",
     "funcionario_id INT references funcionarios(id)",
-    "endereco_id INT references enderecos(id)",
+    "endereco_id INT references enderecos(id) NOT NULL",
     "cliente_id INT references clientes(id)",
     "cortesia_id INT references cortesias(id)",
     "status varchar(50) NOT NULL",
